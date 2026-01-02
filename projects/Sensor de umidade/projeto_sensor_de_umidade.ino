e8c33d469c16a461dff22eaa3b911f5cbb237e90
antony96-severo <antonysevero1@gmail.com>
2026-01-02 00:38:59 -0300

    Add files via upload

---
+++ projects/Sensor de umidade/projeto_sensor_de_umidade.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  Serial.println(analogRead(A0));
+  if (analogRead(A0) >= 300) {
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+  } else {
+    digitalWrite(12, LOW);
+    digitalWrite(13, HIGH);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
